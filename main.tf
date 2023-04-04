d7e207f02c486bd9ba52202a3acc23cbe122b19c
DmytroYurkovskyi <125356146+DmytroYurkovskyi@users.noreply.github.com>
2023-04-04 19:21:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,8 +22,3 @@ resource "google_compute_instance" "dareit-vm-ci" {
     }
   }
 }
-resource "google_storage_bucket_iam_member" "member" {
-    bucket = "dimon-uniquebucket123"
-    role = "roles/storage.admin"
-    member = "allUsers"
-}
