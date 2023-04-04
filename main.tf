9330d88d6d35240057833f7e4d0b74a6289bbb1d
DmytroYurkovskyi <125356146+DmytroYurkovskyi@users.noreply.github.com>
2023-04-04 18:30:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,5 @@
-resource "google_compute_instance" "dareit-vm-ci" {
-  name         = "dareit-vm-tf-ci"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  tags = ["dareit"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        managed_by_terraform = "true"
-      }
-    }
+resource "google_storage_bucket_iam_member" "member" {
+    bucket = "dimon-uniquebucket123"
+    role = "roles/storage.admin"
+    member = "allUsers"
   }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-}
